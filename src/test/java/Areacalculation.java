0f94fbfa87355ce76b49ba6ed82dcdb38b11c445
Madhumitha <madhusedulous@gmail.com>
2021-08-17 08:44:55 -0700

    Checking the area and perimeter of a rectangle with length forty five and breadth thirty six

--- src/test/java/Areacalculation.java
+++ src/test/java/Areacalculation.java
@@ -3,5 +3,14 @@ import org.junit.jupiter.api.Test;
 
 public class Areacalculation {
 
+    @Test
+    void tocheckareaofrectanglewithlengthfortyfiveandbreadththirtysix()
+    {
+        Rectangle rectangle = new Rectangle(45,36);
+        int currentvalue = rectangle.area();
+        int expectedvalue = 1620;
+        Assertions.assertEquals(expectedvalue,currentvalue);
+    }
+
 
 }
--- src/test/java/Perimetercalculation.java
+++ src/test/java/Perimetercalculation.java
@@ -2,4 +2,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Perimetercalculation {
+    @Test
+    void tocheckperimeterofrectanglewithlengthfortyfiveandbreadththirtysix()
+    {
+        Rectangle rectangle = new Rectangle(45,36);
+        int currentvalue = rectangle.perimeter();
+        int expectedvalue = 162;
+        Assertions.assertEquals(expectedvalue,currentvalue);
+    }
+
 }
